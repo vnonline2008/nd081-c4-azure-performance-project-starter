caf44f21d2358291ae75a60f1dedebfa38bcc07e
vnonline2008 <vnonline2008@gmail.com>
2022-12-22 16:05:55 +0700

    fix tracer

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -135,7 +135,13 @@ def index():
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.info('Cats Vote', extra=properties)
+
             vote2 = r.get(button2).decode('utf-8')
+            
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.info('Dogs Vote', extra=properties)
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
